775554db73d8e4f29d057a51b030753089f2e99e
Sparsh <sparshs336@gmail.com>
2021-08-18 21:53:06 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err})
 })
 
-
-app.listen(3000,()=>{
-    console.log('listeing on port 3000');
+const port=process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log(`listining on port${port}`);
 })
